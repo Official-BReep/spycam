16847d779e70351d395799150ce67c0785f78372
Official-BReep <67698695+Official-BReep@users.noreply.github.com>
2022-04-10 22:20:56 +0200

    Create spycam.py

---
+++ spycam.py
@@ -0,0 +1,27 @@
+import cv2
+
+capture = cv2.VideoCapture(0)
+duration = int(input("The Duration of the Video: "))
+duration = duration*10
+fourcc = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
+videoWriter = cv2.VideoWriter('video.avi', fourcc, 30.0, (640, 480))
+current = 0
+limit = 3*duration # bei duration 50 ca. 50 seconds
+
+while(True):
+
+    ret, frame = capture.read()
+
+    if ret:
+        videoWriter.write(frame)
+
+    if current == limit:
+        break
+    else:
+        current += 1
+        print(current)
+
+capture.release()
+videoWriter.release()
+
+cv2.destroyAllWindows()
